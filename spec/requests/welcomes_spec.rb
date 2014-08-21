a2c5902c5d5242c30731dc228e8900819fb897f1
Konkov Mikhail <mikhkonkov@gmail.com>
2014-08-22 01:24:05 +0400

    add layout and fix tests

--- spec/requests/welcomes_spec.rb
+++ spec/requests/welcomes_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe "Welcomes" do
   describe "Welcome page" do
-    it "works! (now write some real specs)" do
-      visit 'welcome/index'
-      expect(page).to have_title('Task manager | Home')
-    end
+    before { visit root_path }
+    subject { page }
+
+    it { should have_title('Task Manager | Home') }
+    it { should have_content('Welcome to task manager') }
   end
 end
