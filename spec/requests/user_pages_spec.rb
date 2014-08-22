15da38b319be89a401f407aef51ee7747131e450
Konkov Mikhail <mikhkonkov@gmail.com>
2014-08-23 00:59:02 +0400

    add simple form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,8 +34,8 @@ describe "UserPages" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "* Password",     with: "foobar"
+        fill_in "Password confirmation", with: "foobar"
       end
 
       it "should create a user" do
