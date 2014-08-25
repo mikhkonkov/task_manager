f2fa4f7fe5b2215d304b6e9c71e3b80511568507
Konkov Mikhail <mikhkonkov@gmail.com>
2014-08-26 00:16:26 +0400

    add update task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,15 @@
 class TasksController < ApplicationController
+  before_action :get_user, except: :show
+  before_action :find_task, only: [ :show, :edit ]
+
   def new
-    @user = current_user
     @task = @user.created_tasks.build
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def create
-    @user = current_user
     @task = TaskCreateType.new(params[:task])
     if @task.save
       flash[:success] = "Task added"
@@ -19,6 +19,28 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @task = TaskCreateType.find(params[:id])
+    if @task.update(params[:task])
+      flash[:success] = "Task updated"
+      redirect_to user_task_path(@user, @task)
+    else
+      render :edit
+    end
+  end
+
   def destroy
+  end
+
+  private
+  def get_user
+    @user = current_user
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
